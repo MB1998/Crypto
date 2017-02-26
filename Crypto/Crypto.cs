8dc69abf6522bb6525594a6f0847919c330cbb1a
nikes <balashovalex858@gmail.com>
2017-02-26 13:57:02 +0200

    Add Berger Code

--- Crypto/Crypto.cs
+++ Crypto/Crypto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Linq;
 using System.Text;
 
 namespace Crypto
@@ -37,7 +38,53 @@ namespace Crypto
             return new BitArray((bool[])encodedData.ToArray(typeof(bool)));
         }
 
-        private static bool Xor(object firstValue, object secondValue) {
+        public static BitArray EncodeBerger(BitArray data)
+        {
+            var temp = new ArrayList();
+            var k = data.Count;
+            var r = Convert.ToInt32(Math.Round(Math.Log(k, 2)));
+            var numOne = 0;
+            for (var i = 0; i < k; i++)
+            {
+                var num = data.Get(i);
+                if (num)
+                    numOne++;
+                temp.Add(num);
+            }
+            var numOneInvers = Convert.ToString(numOne, 2)
+                .PadLeft(r, '0')
+                .Aggregate("", (current, c) => current + (c == '1' ? 0 : 1));
+            foreach (var c in numOneInvers)
+                temp.Add(c == '1');
+            return new BitArray((bool[])temp.ToArray(typeof(bool)));
+        }
+
+        public static BitArray DecodeBerger(BitArray data)
+        {
+            var temp = new ArrayList();
+            var k = data.Count;
+            var r = Convert.ToInt32(Math.Round(Math.Log(k, 2)));
+
+            var oneNum = "";
+            for (var i = data.Count - r; i < data.Count; i++)
+                oneNum += data.Get(i) ? '1' : '0';
+
+            var numOneInvers = oneNum.Aggregate("", (current, c) => current + (c == '1' ? 0 : 1)).PadLeft(8, '0');
+            var countOneInMsg = ToByteArray(numOneInvers)[0];
+            var numOneCheck = 0;
+            for (var i = 0; i < data.Count - r; i++)
+            {
+                var num = data.Get(i);
+                if (num)
+                    numOneCheck++;
+                temp.Add(num);
+            }
+            return numOneCheck == countOneInMsg ? new BitArray((bool[]) temp.ToArray(typeof(bool))) : new BitArray(0);
+        }
+
+
+        private static bool Xor(object firstValue, object secondValue)
+        {
             firstValue = Convert.ToByte(firstValue);
             secondValue = Convert.ToByte(secondValue);
             var temp = (byte)firstValue ^ (byte)secondValue;
@@ -45,15 +92,28 @@ namespace Crypto
             return Convert.ToBoolean(temp);
         }
 
-        public static string BitArrayToStr(BitArray bitArray) {
+        public static string BitArrayToStr(BitArray bitArray)
+        {
             var strArr = new byte[bitArray.Length / 8];
             var encoding = new ASCIIEncoding();
-            for (var i = 0; i < bitArray.Length / 8; i++) {
-                for (int index = i * 8, m = 1; index < i * 8 + 8; index++, m *= 2) {
+            for (var i = 0; i < bitArray.Length / 8; i++)
+            {
+                for (int index = i * 8, m = 1; index < i * 8 + 8; index++, m *= 2)
+                {
                     strArr[i] += bitArray.Get(index) ? (byte)m : (byte)0;
                 }
             }
             return encoding.GetString(strArr);
+        }
+
+        public static byte[] ToByteArray(string str)
+        {
+            var result = Enumerable.Range(0, str.Length / 8).
+                Select(pos => Convert.ToByte(
+                        str.Substring(pos * 8, 8),
+                        2)
+                ).ToArray();
+            return result;
         }
     }
 }
